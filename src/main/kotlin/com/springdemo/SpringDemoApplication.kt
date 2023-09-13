14849f9bea78b2f0921f2296933be3b1537ef2c9
Aqeel-Mohammed <just-dev+Aqeel-Mohammed@tyro.com>
2023-09-13 14:52:52 +1000

    connect to db to insert and fetch messages

--- src/main/kotlin/com/springdemo/SpringDemoApplication.kt
+++ src/main/kotlin/com/springdemo/SpringDemoApplication.kt
@@ -2,7 +2,14 @@ package com.springdemo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.annotation.Id
+import org.springframework.data.jdbc.repository.query.Query
+import org.springframework.data.relational.core.mapping.Table
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
@@ -13,11 +20,33 @@ fun main(args: Array<String>) {
 }
 
 @RestController
-class MessageResource {
+class MessageResource(val service: MessageService) {
 
     @GetMapping
-    fun index(): List<Message> = listOf(Message("1", "Hello"), Message("2", "Salam"))
+    fun index(): List<Message> = service.findMessages()
+
+    @PostMapping
+    fun post(@RequestBody message: Message) {
+        service.post(message)
+    }
+}
+
+@Service
+class MessageService(val db: MessageRepository) {
+
+    fun findMessages(): List<Message> = db.findMessages()
+
+    fun post(message: Message) {
+        db.save(message)
+    }
+}
+
+interface MessageRepository : CrudRepository<Message, String> {
+
+    @Query("select * from messages")
+    fun findMessages(): List<Message>
 
 }
 
-data class Message(val id: String, val text: String)
+@Table("MESSAGES")
+data class Message(@Id val id: String?, val text: String)
