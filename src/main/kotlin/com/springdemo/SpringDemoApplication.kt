a3244e6e52a4268e277a54040b75376fb3e5f6e3
Aqeel-Mohammed <just-dev+Aqeel-Mohammed@tyro.com>
2023-10-11 16:21:16 +1100

    add default cats

--- src/main/kotlin/com/springdemo/SpringDemoApplication.kt
+++ src/main/kotlin/com/springdemo/SpringDemoApplication.kt
@@ -37,8 +37,12 @@ class CatResource(val service: CatService) {
 @Service
 class CatService(val db: JdbcTemplate) {
 
-    fun findCats(): List<Cat> = db.query("select * from cats") { rs, _ ->
-        Cat(rs.getString("id"), rs.getString("name"), rs.getString("image"), rs.getString("description"))
+    fun findCats(): List<Cat> {
+        val fromDb = db.query("select * from cats") { rs, _ ->
+            Cat(rs.getString("id"), rs.getString("name"), rs.getString("image"), rs.getString("description"))
+        }
+
+        return someDefaultCats() + fromDb
     }
 
     fun findCatsById(id: String): List<Cat> = db.query("select * from cats where id = ?", id) { rs, _ ->
@@ -59,3 +63,16 @@ class CatService(val db: JdbcTemplate) {
 data class Cat(val id: String?, val name: String, val image: String, val description: String)
 
 fun String.uuid(): String = UUID.nameUUIDFromBytes(this.encodeToByteArray()).toString()
+
+private fun someDefaultCats(): List<Cat> = listOf(
+    Cat("1", "Whiskers", "https://example.com/cat1.jpg", "A cute fluffy cat."),
+    Cat("2", "Mittens", "https://example.com/cat2.jpg", "Mittens has soft paws."),
+    Cat("3", "Fluffy", "https://example.com/cat3.jpg", "Fluffy is the fluffiest cat."),
+    Cat("4", "Snowball", "https://example.com/cat4.jpg", "Snowball loves to play in the snow."),
+    Cat("5", "Socks", "https://example.com/cat5.jpg", "Socks has colorful socks."),
+    Cat("6", "Oreo", "https://example.com/cat6.jpg", "Oreo has black and white fur."),
+    Cat("7", "Tiger", "https://example.com/cat7.jpg", "Tiger is a fierce hunter."),
+    Cat("8", "Simba", "https://example.com/cat8.jpg", "Simba is the king of the jungle."),
+    Cat("9", "Garfield", "https://example.com/cat9.jpg", "Garfield loves lasagna."),
+    Cat("10", "Felix", "https://example.com/cat10.jpg", "Felix is always getting into mischief.")
+)
